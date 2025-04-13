bffb9fdd2674e6b6b0ef0f6455bc98fd729030ac
Gustavo Porto <Gustavolima0203@gmail.com>
2025-04-13 14:04:15 -0300

    Inicialização da pagina sobre mim

---
+++ Meu Portifolio/src/paginas/sobremim.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SobreMim() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-4">Sobre Mim</h2>
+      <p className="text-lg">Olá! Meu nome é Gustavo Porto e sou estudante de Ciência da Computação. Gosto muito da area de Mateamtica e Lógica. Sou Apaixonado por futebol e torço pro maior time do Nordeste Sport Club do Recife.
+      </p>
+    </div>
+  );
+}
