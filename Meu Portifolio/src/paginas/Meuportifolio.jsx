8d8b09861d478914979cd75fc12f9d8fa07a62e5
Gustavo Porto <Gustavolima0203@gmail.com>
2025-04-13 13:55:46 -0300

    Inicializção da pagina inicial

---
+++ Meu Portifolio/src/paginas/Meuportifolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Inicio() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Bem-vindo ao meu portfólio!</h1>
+      <p className="text-lg">Vou falar um pouco sobre mim e o que gosto de fazer, apresentar alguns projetos ja feitos e implementar o jogo da senha chamado bulls and Cows </p>
+    </div>
+  );
+}
